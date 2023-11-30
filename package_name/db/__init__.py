6b3e077c11a1e773d41d0b2b62b470c9d0b11a08
MohammadrezaAmani <more.amani@yahoo.com>
2023-12-01 00:54:48 +0330

    update module

---
+++ package_name/db/__init__.py
@@ -0,0 +1,33 @@
+from package_name.db.tables import Content, Tag, Type
+from package_name.db.crud import (
+    create_content,
+    get_content,
+    update_content,
+    delete_content,
+    create_tag,
+    get_tag,
+    update_tag,
+    delete_tag,
+    create_type,
+    get_type,
+    update_type,
+    delete_type,
+)
+
+__all__ = [
+    "Content",
+    "Tag",
+    "Type",
+    "create_content",
+    "get_content",
+    "update_content",
+    "delete_content",
+    "create_tag",
+    "get_tag",
+    "update_tag",
+    "delete_tag",
+    "create_type",
+    "get_type",
+    "update_type",
+    "delete_type",
+]
