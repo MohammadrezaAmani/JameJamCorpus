9c6856dc661902fd11c93229e2f1250a9afe04ba
MohammadrezaAmani <more.amani@yahoo.com>
2023-12-01 20:37:54 +0330

    fix bugs. 🌖

--- package_name/__init__.py
+++ package_name/__init__.py
@@ -1,14 +1,29 @@
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from package_name.db import create_tables
 import package_name.config as settings
+from package_name.db.tables import create_tables
+
+# engine = create_async_engine(
+#     settings.DATABASE_URL, echo=settings.DEBUG, future=True
+# )
+# async_session = sessionmaker(
+#     bind=engine,
+#     expire_on_commit=False,
+#     class_=AsyncSession,
+# )
+
+# SESSION = async_session()
+
+from sqlalchemy.orm import scoped_session
 
 
-async def get_session():
-    async_session = sessionmaker(
-        create_async_engine(settings.DATABASE_URL, echo=settings.DEBUG, future=True),
-        expire_on_commit=False,
-        class_=AsyncSession,
+def start() -> scoped_session:
+    # Creating db engine and session
+    engine = create_engine(settings.DATABASE_URL, echo=settings.DEBUG, future=True)
+    create_tables(engine)
+    return scoped_session(
+        sessionmaker(bind=engine, autoflush=False, expire_on_commit=False)
     )
-    async with async_session() as session:
-        yield session
+
+
+SESSION = start()
