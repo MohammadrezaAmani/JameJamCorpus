58e44ba4105a6efa5313eb61876d3b102033eb3f
MohammadrezaAmani <more.amani@yahoo.com>
2023-12-02 20:32:33 +0330

    add 'ProcessPoolExecutor'. 🚀

---
+++ jamejam/scrap.py
@@ -0,0 +1,54 @@
+import asyncio
+import aiohttp
+import logging
+from concurrent.futures import ProcessPoolExecutor
+from jamejam.utils.url import prepare_urls
+from jamejam.utils.process import extract_data
+from jamejam.utils.save import add_to_db
+from jamejam.config import START, END, BASE_URL, MAX_CALLS_PER_SECOND, DEBUG
+
+if DEBUG:
+    logging.basicConfig(level=logging.INFO)
+
+
+async def fetch_data(url: str, semaphore: asyncio.Semaphore):
+    async with semaphore:
+        async with aiohttp.ClientSession() as session:
+            try:
+                async with session.get(url) as response:
+                    if response.status == 200:
+                        text = await response.text()
+                        data = extract_data(text)
+                        data["id"] = int(url.split("/")[-1])
+                        await add_to_db(data)
+                        if DEBUG:
+                            logging.info(f"Data fetched from {url}")
+                    else:
+                        raise Exception(
+                            f"Failed to fetch data from {url}: {response.status}"
+                        )
+            except Exception as e:
+                if DEBUG:
+                    logging.error(
+                        f"Failed to fetch data from {url.split('/')[-1]}: {str(e)[:60 if len(str(e)) > 60 else len(str(e))]}"
+                    )
+
+
+def synchronous_fetch_data(url, semaphore):
+    return asyncio.run(fetch_data(url, semaphore))
+
+
+async def main():
+    urls = prepare_urls(BASE_URL, START, END)
+    semaphore = asyncio.Semaphore(MAX_CALLS_PER_SECOND)
+
+    with ProcessPoolExecutor() as executor:
+        tasks = [
+            executor.submit(synchronous_fetch_data, url, semaphore) for url in urls
+        ]
+        for task in tasks:
+            task.result()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
