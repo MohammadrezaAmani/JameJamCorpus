7955cd80d415a19191f549a7adb3d6ddb6edae63
MohammadrezaAmani <more.amani@yahoo.com>
2023-12-02 12:46:55 +0330

    add guide

--- package_name/__init__.py
+++ package_name/__init__.py
@@ -1,29 +1,46 @@
-from sqlalchemy import create_engine
+import asyncio
 from sqlalchemy.orm import sessionmaker
-import package_name.config as settings
-from package_name.db.tables import create_tables
-
-# engine = create_async_engine(
-#     settings.DATABASE_URL, echo=settings.DEBUG, future=True
-# )
-# async_session = sessionmaker(
-#     bind=engine,
-#     expire_on_commit=False,
-#     class_=AsyncSession,
-# )
-
-# SESSION = async_session()
-
 from sqlalchemy.orm import scoped_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
+from package_name import config
+from package_name.db.tables import BASE
 
 
-def start() -> scoped_session:
-    # Creating db engine and session
-    engine = create_engine(settings.DATABASE_URL, echo=settings.DEBUG, future=True)
-    create_tables(engine)
-    return scoped_session(
-        sessionmaker(bind=engine, autoflush=False, expire_on_commit=False)
+if config.DATABASE_URL.lower().startswith("sqlite"):
+    engine = create_async_engine(
+        config.DATABASE_URL,
+        pool_pre_ping=True,
+        echo=config.DEBUG,
+        pool_recycle=3600,
+        future=True,
+    )
+else:
+    engine = create_async_engine(
+        config.DATABASE_URL,
+        pool_pre_ping=True,
+        pool_size=30,
+        max_overflow=30,
+        echo_pool=config.DEBUG,
+        echo=config.DEBUG,
+        pool_recycle=3600,
+        future=True,
     )
 
 
-SESSION = start()
+def session_factory():
+    return scoped_session(
+        sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+    )
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(BASE.metadata.create_all)
+
+
+asyncio.run(init_db())
+
+
+def get_session():
+    yield session_factory()
