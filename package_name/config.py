cd68e736283e120e32e6217dbc411f6a421b5c5f
MohammadrezaAmani <more.amani@yahoo.com>
2023-12-02 12:47:09 +0330

    add file guid

--- package_name/config.py
+++ package_name/config.py
@@ -1,7 +1,21 @@
-BASE_URL = "https://jamejamonline.ir/fa/news/"
-START = 1
-# END = 1433528
-END = 1433614
+"""Configurations for the package"""
 
-DATABASE_URL = "sqlite:///data.db"
-DEBUG = True
+# ? Base url of the website
+BASE_URL = "https://jamejamonline.ir/fa/news/"
+
+# ? Start and end of dynamic id range
+START = 0
+END = 1433528
+
+# ? Set the maximum number of concurrent calls per second
+MAX_CALLS_PER_SECOND = 1024
+
+# ? Database (postgresql offers better performance)
+#! postgresql
+DATABASE_URL = "postgresql+asyncpg://postgres:password@localhost:5432/db_name"
+
+#! sqlite
+# DATABASE_URL = "sqlite+aiosqlite:///data.db"
+
+# ? Logging
+DEBUG = False
