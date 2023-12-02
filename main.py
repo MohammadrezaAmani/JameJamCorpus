d375fd47f4abd281f9f358787f531b7bfa19bf59
MohammadrezaAmani <more.amani@yahoo.com>
2023-12-02 20:27:03 +0330

    add save to file option.

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from package_name.scrap import main
 import asyncio
+import logging
+from package_name.scrap import main
+from package_name.utils.save import to_file
+from package_name.db.crud import remove_duplicates
 
 if __name__ == "__main__":
+    logging.info("GET DATA...")
     asyncio.run(main())
+
+    logging.info("SOLVE DUPLICATES...")
+    remove_duplicates()
+
+    logging.info("SAVE TO FILE...")
+    asyncio.run(to_file("data.csv"))
