d2ed3bc0c4f681d6d8c3900a63bf8b0537c6809b
MohammadrezaAmani <more.amani@yahoo.com>
2023-12-02 12:48:23 +0330

    fix known bugs

--- package_name/utils/save.py
+++ package_name/utils/save.py
@@ -1,28 +1,26 @@
-
-# from package_name.db import (
-#     async_create_content,
-#     async_type_association,
-#     async_tag_association,
-#     async_create_tag,
-#     async_create_type,
-#     async_tag_association,
-#     async_type_association,
-# )
-from package_name.db.crud2 import (
-    add_content,
-    add_tag,
-    add_type,
-    add_tag_association,
-    add_type_association,
+from package_name import get_session
+from package_name.db import (
+    async_create_content,
+    async_type_association,
+    async_tag_association,
+    async_create_tag,
+    async_create_type,
 )
 
 
 async def add_to_db(data: dict):
+    session = get_session()
+    session = next(session)
     data["timestamp"] = str(data["timestamp"])
-    content_id = add_content(data)
+    content_id = await async_create_content(session=session, **data)
     for tag in data["tags"]:
-        tag_id = add_tag(tag)
-        add_tag_association(content_id.id, tag_id.id)
+        tag_id = await async_create_tag(session=session, name=tag)
+        await async_tag_association(
+            session=session, content_id=content_id.id, tag_id=tag_id.id
+        )
     for typ in data["types"]:
-        type_id = add_type(typ)
-        add_type_association(content_id.id, type_id.id)
+        type_id = await async_create_type(session=session, name=typ)
+        await async_type_association(
+            session=session, content_id=content_id.id, type_id=type_id.id
+        )
+    print("\r", data["id"], end=" ")
